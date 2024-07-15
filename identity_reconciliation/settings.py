2201602c58e2c65848de71e9f11c89b814cc21b4
nimitshrestha <nimitfabshresh@gmail.com>
2024-07-16 05:16:46 +0530

    update installed_apps setting

--- identity_reconciliation/settings.py
+++ identity_reconciliation/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'rest_framework',
+    'contacts',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
