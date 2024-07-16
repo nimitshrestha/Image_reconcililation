e52b4e11787ec546a56bdf785fd14549527d3b86
nimitshrestha <nimitfabshresh@gmail.com>
2024-07-16 07:11:20 +0530

    allowed host

--- identity_reconciliation/settings.py
+++ identity_reconciliation/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['image-reconcililation.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
